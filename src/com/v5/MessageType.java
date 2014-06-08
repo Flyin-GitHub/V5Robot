e0b7e1e98aa97f441edc1b8846ad418455433d84
Felix-GitHub <zf9876@163.com>
2014-06-07 19:41:27 -0700

    first commit

---
+++ src/com/v5/MessageType.java
@@ -0,0 +1,6 @@
+package com.v5;
+
+public enum MessageType {
+	ENEMY_INFO,
+	HEADER_DIED
+}
---
+++ src/com/v5/www/robot/team/RobotInfo.java
@@ -0,0 +1,69 @@
+package com.v5.www.robot.team;
+
+import com.v5.www.robot.Point;
+
+public class RobotInfo {
+	public final static int DISABLE_NOT_FIND_CNT = 15;
+	
+	private String name;
+	private boolean isEnemy;
+	private boolean isEnemyLeader;
+	private Point preLoation;
+	private int priority;
+	private int notFind = 0;
+	
+	@Override
+	public String toString() {
+		return "Name[" + name + "], Priority[" + priority + "]";
+	}
+	
+	public boolean found(String name) {
+		boolean isFound = this.name.equals(name);
+		notFind = (isFound) ? 0 : ++notFind;
+		return isFound;
+	}
+	
+	public boolean isDisable() {
+		return notFind > DISABLE_NOT_FIND_CNT;
+	}
+	
+	public int getPriority() {
+		return priority;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public boolean isEnemy() {
+		return isEnemy;
+	}
+
+	public void setEnemy(boolean isEnemy) {
+		if (isEnemy) 
+			priority += 10;
+		this.isEnemy = isEnemy;
+	}
+
+	public boolean isEnemyLeader() {
+		return isEnemyLeader;
+	}
+
+	public void setEnemyLeader(boolean isEnemyLeader) {
+		if (isEnemyLeader) 
+			priority += 20;
+		this.isEnemyLeader = isEnemyLeader;
+	}
+
+	public Point getPreLoation() {
+		return preLoation;
+	}
+
+	public void setPreLoation(Point preLoation) {
+		this.preLoation = preLoation;
+	}
+}
